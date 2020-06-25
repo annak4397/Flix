2469b6da9e561ed0c70e20bd059289ae0040b9fd
Anna Kuznetsova <annavk@fb.com>
2020-06-24 19:42:57 -0700

    Added detail page for movies

---
+++ Flix/View Controllers/DetailsViewController.h
@@ -0,0 +1,18 @@
+//
+//  DetailsViewController.h
+//  Flix
+//
+//  Created by Anna Kuznetsova on 6/24/20.
+//  Copyright © 2020 Anna Kuznetsova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+@property (nonatomic, strong) NSDictionary *movie;
+
+@end
+
+NS_ASSUME_NONNULL_END
